d3d44ad0aad48f4d865bbf86529e8f9242294d26
mukul571995 <mukul571995@gmail.com>
2017-12-31 11:54:00 +0530

    Express Server Static Files

--- w1/node-express/index.js
+++ w1/node-express/index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const http = require('http');
-
+const morgan = require('morgan')
 
 // server configurartions
 const hostname = 'localhost';
 const port = '8080'
 
 // We are going to use express in our app
-const app = express()
+const app = express();
+
+// user morgan for logging, dev provide additional information
+app.use(morgan('dev'));
+
+// serving html files using express
+app.use(express.static(__dirname+'/public'));
 
 // Our app
 app.use((req, res, next) => {
-    console.log(req.headers);
+
+    // now we don't need to log the whole request, morgna will provide
+    // us relevant looging 
+    /*console.log(req.headers);*/
 
     res.statusCode = '200';
     res.setHeader('Content-Type', 'text/html');
