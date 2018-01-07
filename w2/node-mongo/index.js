8720aef7d042dc40b1c8c4800664bf6bc6b73dbd
mukul571995 <mukul571995@gmail.com>
2018-01-08 03:45:01 +0530

    Node Callback Hell and Promises

--- w2/node-mongo/index.js
+++ w2/node-mongo/index.js
@@ -8,53 +8,42 @@ const dbOperations = require("./operation");
 const url = 'mongodb://localhost:27017/conFusion';
 
 // getting connection to mongodb
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url).then((client) => {
     // Checking if successfully connected to server
-    assert.equal(err, null);
     console.log("Connected successfully to conFusion");
 
-    /******* From stackoverflow, i was getting db.collection is not a method error
-    /In version 2.x of the MongoDB native NodeJS driver you would get the database object as an argument to the connect callback:
-
-    MongoClient.connect('mongodb://localhost:27017/mytestingdb', (err, db) => {
-      // Database returned
-    });
-    According to the changelog for 3.0 you now get a client object containing the database object instead:
-    
-    MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-      // Client returned
-      var db = client.db('mytestingdb');
-    });
-     */
     const conFusionDb = client.db('conFusion');
 
     dbOperations.insertDocument(conFusionDb, {'name':'panipuri', 'description': 'really good'},
-            "dishes", (result) => {
-        console.log("Inserted document:\n " , result.ops);
-
-        dbOperations.findDocuments(conFusionDb, "dishes", (docs) => {
+        "dishes")
+        .then((result) => {
+            console.log("Inserted document:\n " , result.ops);
+            return dbOperations.findDocuments(conFusionDb, "dishes");
+        })       
+        .then((docs) => {
             console.log("Found documents:\n " , docs); 
-               
-            dbOperations.updateDocument(conFusionDb, {'name':'panipuri'}, {'description': 'five stars'},"dishes",
-                   (result) => {
-                console.log("Documents updated:\n" , result.result); 
-
-                dbOperations.findDocuments(conFusionDb, "dishes", (docs) => {
-                    console.log("Found updated documents:\n " , docs); 
-                        
-                    dbOperations.removeDocument(conFusionDb,{'name':'panipuri'},
-                     "dishes", (result) => {
-                        console.log("Deleted the document\n", result);
-
-                        conFusionDb.dropCollection("dishes", (result) => {
-                            console.log("Dropped collection: \n", result);
-                            client.close();
-                        });
-                    });
-                });
-            });
-        });
-    });
-});
+            return dbOperations.updateDocument(conFusionDb, {'name':'panipuri'},{'description': 'five stars'},"dishes");
+        })       
+        .then((result) => {
+            console.log("Documents updated:\n" , result.result);
+            return dbOperations.findDocuments(conFusionDb, "dishes"); 
+        })               
+        .then((docs)=> {
+            console.log("Found updated documents:\n " , docs);
+            dbOperations.removeDocument(conFusionDb,{'name':'panipuri'},"dishes");
+        })                         
+        .then((result) => {
+            console.log("Deleted the document\n");
+            return conFusionDb.dropCollection("dishes");
+        })          
+        .then((result)=> {
+            console.log("Dropped collection: \n", result);
+            client.close();
+        })
+        .catch((err) =>{
+            console.log("Error: "+err);
+        })
+})
+.catch((err) => {console.log("Error:" + err)});
 
 
--- w2/node-mongo/operation.js
+++ w2/node-mongo/operation.js
@@ -5,42 +5,20 @@ const assert = require("assert");
 
 exports.insertDocument = (db, document, collection, callback) => {
     const call = db.collection(collection);
-
-    call.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("inserted "+result.result.n+" documents into the collection "+collection);
-        callback(result);
-    });
+    return call.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const call = db.collection(collection);
-    
-    call.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        
-        callback(docs);
-    });
+    return call.find({}).toArray();
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const call = db.collection(collection);
-
-    call.updateOne(document, {$set: update}, null, (err, result) => {
-        assert.equal(err, null);
-
-        // update here is a javascript object so '+' is not used
-        console.log("update ", update);
-        callback(result);
-    });
+    return call.updateOne(document, {$set: update}, null);
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const call = db.collection(collection);
-    call.deleteOne(document, (err, result) => {
-
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-        callback(result);        
-    });
+    return call.deleteOne(document);
 };
